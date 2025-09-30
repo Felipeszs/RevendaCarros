4e2b3e690bd72805b7ca67e13ee96c071f777d50
Felipe Souza <felipemedeirossouza1@gmail.com>
2025-09-30 10:11:06 -0300

    First commit

---
+++ cars/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='Car_brand')
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.model
+    
